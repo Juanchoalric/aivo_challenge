2e7e34af6ac121bbe42dd2894c5b5b10af8b54b7
juan <jalric@andreani.com>
2021-05-19 22:29:17 -0300

    Added schema for life_satisfaction_index

--- app/services/country_filter_service.py
+++ app/services/country_filter_service.py
@@ -1,12 +1,18 @@
+import pandas as pd
+
 class CountryFilterService:
     @classmethod
-    def country_filter(cls, df, params):
+    def country_filter(cls, params):
         """
-        
+
         Filter the given Dataframe if the value given is greater than the value stored
         and check for Total Inequality
         """
-        df_filtered = df[(df["Value"] > float(params["value"])) & (df["Inequality"] == "Total")]
+        df_life_satisfaction = __read_from_countries_dataset()
+        df_filtered = df_life_satisfaction[
+            (df_life_satisfaction["Value"] > float(params["life_satisfaction_index"])) & 
+            (df_life_satisfaction["Inequality"] == "Total")
+            ]
         response = {'countries': []}
         for _, row in df_filtered.iterrows():
             response["countries"].append(
@@ -23,3 +29,6 @@ class CountryFilterService:
                 }
             )
         return response
+
+    def __read_from_countries_dataset():
+        return pd.read_csv("app\countries.csv")
